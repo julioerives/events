a20540c8b9788d03376b1113e7378e5effbeb427
JulioErives <julioerives345@gmail.com>
2025-04-18 14:47:22 -0700

    feat: add interceptor

---
+++ src/app/core/interceptors/interceptor.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+
+export const interceptorInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        alert(error.error?.message)
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  );
+};
